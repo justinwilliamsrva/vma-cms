cb9c522a6d77f2646922259e4903c3a680954b52
Justin Williams <justinwdev@gmail.com>
2024-04-05 23:43:21 -0400

    Create Hamburger.

---
+++ resources/js/Components/Hamburger.vue
@@ -0,0 +1,28 @@
+<script setup>
+    import {ref} from 'vue';
+    const showingNavigationDropdown = ref(false);
+
+    function toggleMenu() {
+        showingNavigationDropdown.value = !showingNavigationDropdown.value;
+    }
+</script>
+<template>
+    <div class="w-12 flex lg:hidden items-center">
+        <div @click="toggleMenu" class="flex-1 px-2 h-3 flex flex-col justify-between items-center cursor-pointer">
+            <!-- Hamburger line 1 -->
+            <div
+                :class="[
+                    'w-full h-[1px] bg-black transition-transform duration-300 ease-in-out',
+                    showingNavigationDropdown ? 'rotate-[135deg] translate-y-1.5' : ''
+                ]"
+            ></div>
+            <!-- Hamburger line 2 -->
+            <div
+                :class="[
+                    'w-full h-[1px] bg-black transform transition duration-300 ease-in-out',
+                    showingNavigationDropdown ? '-rotate-[135deg] -translate-y-1.5' : ''
+                ]"
+            ></div>
+        </div>
+    </div>
+</template>
--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -8,6 +8,7 @@ import NavDropdown from '@/Components/NavDropdown.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
 import SpecialButton from '@/Components/SpecialButton.vue';
 import MainNavLink from '@/Components/MainNavLink.vue';
+import Hamburger from '@/Components/Hamburger.vue';
 import Footer from '@/Components/Footer.vue';
 import { Link, Head } from '@inertiajs/vue3';
 import imageUrl from '@assets/images/VMA+Black+Logo.png';
@@ -19,8 +20,6 @@ const aboutUsRoutes = [
     {'text':'Location', 'url':'https://www.villagemont.org/location'},
     {'text':'Calendar', 'url':'https://www.villagemont.org/calendar'},
 ];
-
-const showingNavigationDropdown = ref(false);
 </script>
 
 <template>
@@ -28,87 +27,86 @@ const showingNavigationDropdown = ref(false);
 
     <div>
         <div class="min-h-screen bg-background-primary flex flex-col">
-            <nav class="px-[4%] py-[2%]">
+            <nav class="p-[6%] px-lg:px-[4%] lg:py-[2%]">
                 <!-- Primary Navigation Menu -->
-                <div class="">
-                    <div class="flex w-full">
-                        <!-- Navigation Links -->
-                        <div class="hidden space-x-4 lg:flex items-center flex-1">
-                            <MainNavLink :href="'https://www.villagemont.org/'">
-                                Home
+                <div class="flex w-full">
+                    <!-- Navigation Links -->
+                    <div class="hidden lg:flex space-x-4 items-center flex-1">
+                        <MainNavLink :href="'https://www.villagemont.org/'">
+                            Home
+                        </MainNavLink>
+                        <NavDropdown>
+                            <template #trigger>
+                                About Us
+                            </template>
+                            <MainNavLink v-for="route in aboutUsRoutes" :key="route.text" :href="route.url">
+                                {{ route.text }}
                             </MainNavLink>
-                            <NavDropdown>
-                                <template #trigger>
-                                    About Us
-                                </template>
-                                <MainNavLink v-for="route in aboutUsRoutes" :key="route.text" :href="route.url">
-                                    {{ route.text }}
-                                </MainNavLink>
-                            </NavDropdown>
-                            <MainNavLink :href="'https://www.villagemont.org/programs'">
-                                Program
-                            </MainNavLink>
-                            <MainNavLink :href="'https://www.villagemont.org/admission'">
-                                Admission
-                            </MainNavLink>
-                            <NavLink :href="route('portal')" :active="route().current('portal')">
-                                Portal
-                            </NavLink>
-                        </div>
-
-                        <!-- Logo -->
-                        <div class="shrink-0 flex items-center justify-center">
-                            <a href="https://www.villagemont.org">
-                               <img :src="imageUrl" class="w-42">
-                            </a>
-                        </div>
-
-                        <div class="flex-1 flex items-center justify-end">
-                            <SpecialButton :href="route('landing-page')" class="px-9 py-5 text-sm">
-                                Inquire Here!
-                            </SpecialButton>
-                        </div>
-
-                        <!-- Hamburger -->
-                        <div class="-me-2 flex items-center lg:hidden">
-                            <button
-                                @click="showingNavigationDropdown = !showingNavigationDropdown"
-                                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
-                            >
-                                <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                                    <path
-                                        :class="{
-                                            hidden: showingNavigationDropdown,
-                                            'inline-flex': !showingNavigationDropdown,
-                                        }"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M4 6h16M4 12h16M4 18h16"
-                                    />
-                                    <path
-                                        :class="{
-                                            hidden: !showingNavigationDropdown,
-                                            'inline-flex': showingNavigationDropdown,
-                                        }"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M6 18L18 6M6 6l12 12"
-                                    />
-                                </svg>
-                            </button>
-                        </div>
+                        </NavDropdown>
+                        <MainNavLink :href="'https://www.villagemont.org/programs'">
+                            Program
+                        </MainNavLink>
+                        <MainNavLink :href="'https://www.villagemont.org/admission'">
+                            Admission
+                        </MainNavLink>
+                        <NavLink :href="route('portal')" :active="route().current('portal')">
+                            Portal
+                        </NavLink>
                     </div>
+
+                    <!-- Logo -->
+                    <div class="pl-12 lg:pl-0 flex-grow lg:shrink-0 flex items-center justify-center">
+                        <a href="https://www.villagemont.org">
+                            <img :src="imageUrl" class="w-24 lg:w-42">
+                        </a>
+                    </div>
+
+                    <div class="flex-1 hidden lg:flex items-center justify-end">
+                        <SpecialButton :href="route('landing-page')" class="px-9 py-5 text-sm">
+                            Inquire Here!
+                        </SpecialButton>
+                    </div>
+
+                    <!-- Hamburger -->
+                    <Hamburger />
+
+                    <!-- <div class="flex items-center lg:hidden">
+                        <button
+                            @click="showingNavigationDropdown = !showingNavigationDropdown"
+                            class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+                        >
+                            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                                <path
+                                    :class="{
+                                        hidden: showingNavigationDropdown,
+                                        'inline-flex': !showingNavigationDropdown,
+                                    }"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M4 6h16M4 12h16M4 18h16"
+                                />
+                                <path
+                                    :class="{
+                                        hidden: !showingNavigationDropdown,
+                                        'inline-flex': showingNavigationDropdown,
+                                    }"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M6 18L18 6M6 6l12 12"
+                                />
+                            </svg>
+                        </button>
+                    </div> -->
                 </div>
 
                 <!-- Responsive Navigation Menu -->
                 <div
                     :class="{ block: showingNavigationDropdown, hidden: !showingNavigationDropdown }"
-                    class="sm:hidden"
                 >
                     <div class="pt-2 pb-3 space-y-1">
-                        <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
+                        <ResponsiveNavLink :href="'https://www.villagemont.org/'">
                             Dashboard
                         </ResponsiveNavLink>
                     </div>
@@ -133,11 +131,11 @@ const showingNavigationDropdown = ref(false);
             </nav>
 
             <!-- Page Heading -->
-            <header class="bg-white shadow" v-if="$slots.header">
+            <!-- <header class="bg-white shadow" v-if="$slots.header">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     <slot name="header" />
                 </div>
-            </header>
+            </header> -->
 
             <!-- Page Content -->
             <main class="flex-grow bg-background-secondary">
